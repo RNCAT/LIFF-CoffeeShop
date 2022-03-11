5db198f518a68d9d598881010f342cfc1ca8e688
RNCAT <jaruwat.works@gmail.com>
2022-03-11 21:19:56 +0700

    :lipstick: update col size

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-sm-12 col-md-4">
+      <div class="col-md-3"></div>
+      <div class="col-sm-12 col-md-6">
         <router-view />
       </div>
-      <div class="col-md-4"></div>
+      <div class="col-md-3"></div>
     </div>
   </div>
 </template>
-
